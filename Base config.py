3ec4185de10ef7f9716dc36593a4473f3f2f27c2
Peter Pluvkov <ppluvkov@gmail.com>
2023-02-10 16:17:41 +0300

    conf 2 realise

--- Base config.py
+++ Base config.py
@@ -1,4 +1,6 @@
 """
+CONF 1
+
 response = openai.Completion.create(
         model="text-davinci-003",
         prompt=message.text,
@@ -7,6 +9,20 @@ response = openai.Completion.create(
         top_p=1,
         frequency_penalty=0.0,
         presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+    )
+
+
+CONF 2
+
+response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message.text,
+        temperature=0.3,
+        max_tokens=1500,
+        top_p=0.3,
+        frequency_penalty=0.0,
+        presence_penalty=0.3,
         stop=[" Human:", " AI:"]
     )
 """
