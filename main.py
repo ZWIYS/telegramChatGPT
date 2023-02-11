223aa3c4086daa5190912d292c6c0cdae7fb1c84
Peter Pluvkov <ppluvkov@gmail.com>
2023-02-11 13:53:24 +0300

    config 3 beta

--- main.py
+++ main.py
@@ -16,13 +16,14 @@ async def send(message: types.Message):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=message.text,
-        temperature=0.01,
+        temperature=0.4,
         max_tokens=2048,
-        top_p=0.1,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
+        top_p=0.3,
+        frequency_penalty=0.7,
+        presence_penalty=0.3,
         stop=[" Human:", " AI:"]
     )
+    
     await message.answer(response['choices'][0]['text'])
 
 executor.start_polling(dp, skip_updates=True)
