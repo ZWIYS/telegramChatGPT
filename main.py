85dfa151c077ca018b6e67d825bd16c9f40b544f
Peter Pluvkov <ppluvkov@gmail.com>
2023-02-26 11:24:13 +0300

    config test

--- main.py
+++ main.py
@@ -35,11 +35,11 @@ async def send(message: types.Message):
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=message.text,
-            temperature=0,
+            temperature=0.3,
             max_tokens=2048,
-            top_p=0,
+            top_p=1,
             frequency_penalty=0.0,
-            presence_penalty=0.0,
+            presence_penalty=0.6,
             stop=[" Human:", " AI:"]
         )
 
