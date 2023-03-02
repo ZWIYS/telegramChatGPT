84591d0491e7840860ce48028cee3b5cd12a98d5
Peter Pluvkov <ppluvkov@gmail.com>
2023-03-02 18:31:49 +0300

    conf edit

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ async def send(message: types.Message):
             temperature=0,
             max_tokens=3500,
             top_p=0,
-            frequency_penalty=2,
-            presence_penalty=2,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
             stop=[" Human:", " AI:"]
         )
 
